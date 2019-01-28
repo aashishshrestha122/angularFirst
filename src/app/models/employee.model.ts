a9098c8626bf90a5b4cf51ff3fac619a678a4ae1
aashishshrestha122 <aashishshrestha12@gmail.com>
2019-01-28 18:52:33 +0545

    first commit

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee{
+    id: Number;
+    name: string;
+    gender: string;
+    email?: string;
+    phoneNumber?: number;
+    contactPreference: string;
+    dateOfBirth: Date;
+    department: string;
+    isActive: boolean;
+    photoPath?: string;
+
+}
